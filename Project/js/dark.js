598c739650dd265f20364f63ecafe78219330542
Gigashmaru <golyholy05@gmail.com>
2025-07-18 08:42:16 -0700

    Project

--- Project/js/dark.js
+++ Project/js/dark.js
@@ -2,10 +2,10 @@
 let modeBtn = document.querySelector(".dark-mode");
 
 modeBtn.addEventListener("click", function () {
-  if (document.body.className != "dark") {
+  if (document.body.className != "skip") {
     this.firstElementChild.src = "./images/sun.svg";
   } else {
-    this.firstElementChild.src = "./images/moon.svg";
+    this.firstElementChild.src = "images/moon.svg";
   }
-  document.body.classList.toggle("dark");
+  document.body.classList.toggle("skip");
 });
